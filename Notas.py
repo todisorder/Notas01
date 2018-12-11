1c397aa83fd9a72bbd3f8ceae38952a4ea1ebf7b
todisorder <pauloamo@gmail.com>
2018-12-10 23:02:23 -0200

    excel

--- Notas.py
+++ Notas.py
@@ -1,7 +1,20 @@
-isto
+import xlsxwriter as xl
 
-"quecena"
-"amamam"
-"hahah"
+workbook = xl.Workbook('demo.ods')
+# o libre office não lê isto...
+worksheet = workbook.add_worksheet()
 
-sdf
+# Widen the first column to make the text clearer.
+worksheet.set_column('A:A', 20)
+
+# Write some simple text.
+worksheet.write('A1', 'Hello')
+
+# Write some numbers, with row/column notation.
+worksheet.write(2, 0, 123)
+worksheet.write(3, 0, 123.456)
+
+worksheet.write_formula('A1', '=SUM(1, 2, 3)')
+
+
+workbook.close()
